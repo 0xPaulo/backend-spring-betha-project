c35b310e1b75fc758375a48ced1606d41fec35db
0xPaulo <paulo_czar@outlook.com.br>
2024-02-10 10:04:53 -0300

    CrossOrigin fix

--- src/main/java/com/betha/backend/controller/ManutencaoController.java
+++ src/main/java/com/betha/backend/controller/ManutencaoController.java
@@ -2,6 +2,7 @@ package com.betha.backend.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.betha.backend.repository.CadastroRepository;
 import lombok.AllArgsConstructor;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/lista")
 @AllArgsConstructor
 public class ManutencaoController {

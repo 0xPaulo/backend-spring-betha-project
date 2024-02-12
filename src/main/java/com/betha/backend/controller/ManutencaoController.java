dde53cb1fd9ed21d268dcf37a48e0c8b433eb4eb
0xPaulo <paulo_czar@outlook.com.br>
2024-02-12 05:41:37 -0300

    post-cadastro-front=>back=>front

--- src/main/java/com/betha/backend/controller/ManutencaoController.java
+++ src/main/java/com/betha/backend/controller/ManutencaoController.java
@@ -2,8 +2,12 @@ package com.betha.backend.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +27,16 @@ public class ManutencaoController {
   @GetMapping
   public List<Cadastro> list() {
     return cadastroRepository.findAll();
+  }
+
+  // @SuppressWarnings("null")
+  @SuppressWarnings("null")
+  @PostMapping
+  public ResponseEntity<Cadastro> create(@RequestBody Cadastro cadastro) {
+    System.out.println(cadastro);
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(cadastroRepository.save(cadastro));
+
   }
 
 }

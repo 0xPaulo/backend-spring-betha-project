575788fb18f3aa7948342a32ed9f7c28d2a16329
0xPaulo <paulo_czar@outlook.com.br>
2024-02-18 13:03:49 -0300

    novo banco

--- src/main/java/com/betha/backend/model/Cadastro.java
+++ src/main/java/com/betha/backend/model/Cadastro.java
@@ -1,9 +1,15 @@
 package com.betha.backend.model;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,10 +26,26 @@ public class Cadastro {
   @JsonProperty("_id")
   private Long id;
 
-  @Column(name = "Item", length = 200, nullable = false)
+  @Column(name = "Nome_Cliente", length = 200, nullable = false)
   private String name;
 
-  @Column(name = "Defeito", length = 200, nullable = false)
+  @Column(name = "Item", length = 200, nullable = false)
+  private String item;
+
+  @Column(name = "Defeito_item", nullable = false)
   private String defeito;
 
+  @Column(name = "Data_Entrada")
+  @DateTimeFormat(pattern = "dd/MM/yyyy")
+  private LocalDate dataEntrada = LocalDate.now();
+
+  @Column(name = "Data_Saida")
+  @DateTimeFormat(pattern = "dd/MM/yyyy")
+  private LocalDate dataSaida = LocalDate.now();
+
+  @Column(name = "Valor")
+  private int valor;
+
+  @Enumerated(EnumType.STRING)
+  private com.betha.backend.Enum.Status status;
 }
